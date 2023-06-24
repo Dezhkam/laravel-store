67c5e019fba2038eb30ca4bcc2c84c5bc58fcf75
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-06-24 10:14:52 +0330

    198 - create payment & product page

--- resources/views/admin/market/order/index.blade.php
+++ resources/views/admin/market/order/index.blade.php
@@ -13,8 +13,6 @@
       <li class="breadcrumb-item font-size-12 active" aria-current="page"> سفارشات</li>
     </ol>
   </nav>
-
-
   <section class="row">
     <section class="col-12">
         <section class="main-body-container">
