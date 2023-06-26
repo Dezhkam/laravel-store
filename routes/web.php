e840ad8234253e677ac945446ef903f539dcd288
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-06-26 06:19:45 +0330

    202 - create faq and menu pages

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\Admin\Market\DiscountController;
 use App\Http\Controllers\Admin\Market\PropertyController;
 use App\Http\Controllers\Admin\Content\CommentController as ContentCommentController;
 use App\Http\Controllers\Admin\Content\CategoryController as ContentCategoryController;
+use App\Http\Controllers\Admin\Content\MenuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -177,11 +178,20 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
       //faq
       Route::prefix('faq')->group(function(){
         Route::get('/', [FAQController::class, 'index'])->name('admin.content.faq.index');
-        Route::get('/create', [FAQController::class, 'create'])->name('admin.content.faq.show');
+        Route::get('/create', [FAQController::class, 'create'])->name('admin.content.faq.create');
         Route::post('/store', [FAQController::class, 'store'])->name('admin.content.faq.store');
         Route::get('/edit/{id}', [FAQController::class, 'edit'])->name('admin.content.faq.edit');
         Route::put('/update/{id}', [FAQController::class, 'update'])->name('admin.content.faq.update');
         Route::delete('/destroy/{id}', [FAQController::class, 'destroy'])->name('admin.content.faq.destroy');
+});
+  //faq
+      Route::prefix('menu')->group(function(){
+        Route::get('/', [MenuController::class, 'index'])->name('admin.content.menu.index');
+        Route::get('/create', [MenuController::class, 'create'])->name('admin.content.menu.create');
+        Route::post('/store', [MenuController::class, 'store'])->name('admin.content.menu.store');
+        Route::get('/edit/{id}', [MenuController::class, 'edit'])->name('admin.content.menu.edit');
+        Route::put('/update/{id}', [MenuController::class, 'update'])->name('admin.content.menu.update');
+        Route::delete('/destroy/{id}', [MenuController::class, 'destroy'])->name('admin.content.menu.destroy');
 });
 
 
