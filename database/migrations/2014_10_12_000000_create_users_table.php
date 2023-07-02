627304f792930deb23308605f4b7407b938f168a
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-07-02 08:05:48 +0330

    210 - install jetstream

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,30 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique()->nullable();
             $table->string('mobile')->unique()->nullable();
+            $table->string('password');
             $table->string('national_code')->unique()->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('slug')->unique()->nullable();
             $table->text('profile_photo_path')->nullable()->comment('avatar');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->tinyInteger('activation')->default(0)->comment('0 => inactive, 1 => active');
             $table->timestamp('activation_date')->nullable();
             $table->tinyInteger('user_type')->default(0)->comment('0 => user, 1 => admin');
             $table->tinyInteger('status')->default(0);
+            $table->foreignId('current_team_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
@@ -36,11 +35,9 @@ class CreateUsersTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
-}
+};
