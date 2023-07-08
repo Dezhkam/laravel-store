abb360c1a8f81d3a67f4d0f0af74ac9785349271
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-07-08 11:36:21 +0330

    239 - create and edit pages of postCategories - Add select2 tags

--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -37,75 +37,83 @@
                         <section class="col-12 col-md-6 my-2">
                             <div class="form-group">
                                 <label for="name">نام دسته</label>
-                                <input type="text" class="form-control form-control-sm" name='name' id='name' value="{{ old('name') }}">
+                                <input type="text" class="form-control form-control-sm" name="name" id="name" value="{{ old('name') }}">
                             </div>
                             @error('name')
-                                <span class='alert_required bg-danger text-white p-1 rounded' role="alert">
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
                                     <strong>
                                         {{ $message }}
                                     </strong>
                                 </span>
                             @enderror
                         </section>
+
                         <section class="col-12 col-md-6 my-2">
                             <div class="form-group">
                                 <label for="tags">تگ ها</label>
-                                <input type="hidden" class="form-control form-control-sm" name='tags' id='tags' value="{{ old('tags') }}">
-                                <select id="select_tags" name="" class="select2 form-control form-control-sm" multiple>
+                                <input type="hidden" class="form-control form-control-sm"  name="tags" id="tags" value="{{ old('tags') }}">
+                                <select class="select2 form-control form-control-sm" id="select_tags" multiple>
 
                                 </select>
                             </div>
                             @error('tags')
-                                <span class='alert_required bg-danger text-white p-1 rounded' role="alert">
-                                    <strong>
-                                        {{ $message }}
-                                    </strong>
-                                </span>
-                            @enderror
+                            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
                         </section>
 
                         <section class="col-12 col-md-6 my-2">
                             <div class="form-group">
                                 <label for="status">وضعیت</label>
-                                <select name="status" id="" class="form-control form-control-sm" id='status' >
-                                    <option value="0" @if (old('status') == 0) selected @endif>غیرفعال</option>
-                                    <option value="1" @if (old('status') == 1) selected @endif>فعال</option>
+                                <select name="status" id="" class="form-control form-control-sm" id="status">
+                                    <option value="0" @if(old('status') == 0) selected @endif>غیرفعال</option>
+                                    <option value="1" @if(old('status') == 1) selected @endif>فعال</option>
                                 </select>
                             </div>
                             @error('status')
-                                <span class='alert_required bg-danger text-white p-1 rounded' role="alert">
-                                    <strong>
-                                        {{ $message }}
-                                    </strong>
-                                </span>
-                            @enderror
+                            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
                         </section>
+
                         <section class="col-12 col-md-6 my-2">
                             <div class="form-group">
                                 <label for="image">تصویر</label>
-                                <input type="file" class="form-control form-control-sm" name='image' id='image'  >
+                                <input type="file" class="form-control form-control-sm" name="image" id="image">
                             </div>
                             @error('image')
-                                <span class='alert_required bg-danger text-white p-1 rounded' role="alert">
-                                    <strong>
-                                        {{ $message }}
-                                    </strong>
-                                </span>
-                            @enderror
+                            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
                         </section>
-                        <section class="col-12 col-md-6 my-2">
+
+
+                        <section class="col-12">
                             <div class="form-group">
-                                <label for="description">توضیحات</label>
-                                <textarea name="description" id="description"  class="form-control form-control-sm" rows="6">{{ old('description') }}</textarea>
+                                <label for="">توضیحات</label>
+                                <textarea name="description" id="description"  class="form-control form-control-sm" rows="6">
+                                    {{ old('description') }}
+                                </textarea>
                             </div>
                             @error('description')
-                                <span class='alert_required bg-danger text-white p-1 rounded' role="alert">
-                                    <strong>
-                                        {{ $message }}
-                                    </strong>
-                                </span>
-                            @enderror
+                            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
                         </section>
+
+
                         <section class="col-12 my-3">
                             <button class="btn btn-primary btn-sm">ثبت</button>
                         </section>
@@ -118,26 +126,41 @@
 </section>
 
 @endsection
+
 @section('script')
 
     <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
     <script>
         CKEDITOR.replace('description');
     </script>
+
     <script>
-       $(document).ready(function() {    
-            var tags_input =  $('#tags');
+        $(document).ready(function () {
+            var tags_input = $('#tags');
             var select_tags = $('#select_tags');
+            var default_tags = tags_input.val();
+            var default_data = null;
+
+            if(tags_input.val() !== null && tags_input.val().length > 0)
+            {
+                default_data = default_tags.split(',');
+            }
+
             select_tags.select2({
-                placeholder: 'لطفا تگ های خود را وارد نمایید',
+                placeholder : 'لطفا تگ های خود را وارد نمایید',
                 tags: true,
+                data: default_data
             });
-            $('#form').submit(function (event){
-                if(select_tags.val() !== null && select_tags.val().length >0  ){
+            select_tags.children('option').attr('selected', true).trigger('change');
+
+
+            $('#form').submit(function ( event ){
+                if(select_tags.val() !== null && select_tags.val().length > 0){
                     var selectedSource = select_tags.val().join(',');
-                    tags_input.val(selectedSource);
+                    tags_input.val(selectedSource)
                 }
-            });
-        });
+            })
+        })
     </script>
+
 @endsection
