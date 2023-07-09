6f94e7ca85bdf1ae056f31092de6810384fc79f4
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-07-09 21:10:47 +0330

    248 - add sluggable persian package - composer require pishran/laravel-persian-slug - and sluggable postCategory Model

--- app/Models/Content/PostCategory.php
+++ app/Models/Content/PostCategory.php
@@ -2,12 +2,20 @@
 
 namespace App\Models\Content;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Pishran\LaravelPersianSlug\HasPersianSlug;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PostCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes,HasPersianSlug;
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
     protected $fillable = ['name','description','slug','image','status','tags'];
 }
