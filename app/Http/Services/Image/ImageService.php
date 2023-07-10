3dbea40809a37cbf05a3bc5ddcec373e53c6a3cd
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-07-10 20:29:05 +0330

    255 - complete ImageService.php

--- app/Http/Services/Image/ImageService.php
+++ app/Http/Services/Image/ImageService.php
@@ -59,7 +59,31 @@ class ImageService extends ImageToolsService{
         $image['indexArray'] = $indexArray;
         $image['directory'] = $this->getFinalImageDirectory();
         $image['currentImage'] = Config::get('image.default-current-index-image'); 
-        
+
         return $image;
+    }
+    public function deleteImage($imagePath){
+        if(file_exists($imagePath)){
+            unlink($imagePath);
+        }
+    }
+    public function deleteIndex($images){
+        $directory = public_path($images['directory']);
+        $this->deleteDirectoryAndFiles($directory);
+    }
+    public function deleteDirectoryAndFiles($directory){
+        if(!is_dir($directory)){
+            return false;
+        }
+        $files = glob($directory . DIRECTORY_SEPARATOR . '*' , GLOB_MARK);
+        foreach($files as  $file){
+            if(is_dir($file)){
+                $this->deleteDirectoryAndFiles($file);
+            }else{
+                unlink($file);
+            }
+        }
+        $result = rmdir($directory);
+        return $result;
     }
 }
