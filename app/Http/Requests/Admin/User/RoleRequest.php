c794a885e9bfa446dcb0796a81700c507c9eda00
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-07-19 18:46:31 +0330

    292 - create page for Role with permissions

---
+++ app/Http/Requests/Admin/User/RoleRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Admin\User;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        $route = Route::current();
+        if($route->getName() === 'admin.user.role.store'){
+            return [
+                'name' => 'required|max:120|min:1',
+                'description' => 'required|max:200|min:1',
+                'permissions.*' => ['exists:permissions,id']
+            ];
+        }
+    }
+    public function attributes(){
+        return [
+            // overwrite on values in validation fa language file
+            'name' => 'عنوان نقش',
+            'permissions.*' => 'دسترسی',
+        ];
+    }
+}
