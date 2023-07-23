d9e5fcc1358bc6b3f0b629059116fa50133b034d
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-07-23 15:39:25 +0330

    304 - Complete Comment pages for market

--- app/Http/Controllers/Admin/Content/CommentController.php
+++ app/Http/Controllers/Admin/Content/CommentController.php
@@ -16,12 +16,13 @@ class CommentController extends Controller
      */
     public function index()
     {
-        $unSeenComments = Comment::where('seen',0)->get();
+
+        $unSeenComments = Comment::where('commentable_type','App\Models\Content\Post')->where('seen',0)->get();
         foreach ($unSeenComments as $unSeenComment) {
             $unSeenComment->seen = 1;
             $result = $unSeenComment->save();
         }
-        $comments = Comment::orderBy('created_at','desc')->simplePaginate(15);
+        $comments = Comment::orderBy('created_at','desc')->where('commentable_type','App\Models\Content\Post')->simplePaginate(15);
         return view('admin.content.comment.index',compact('comments'));
 
     }
@@ -104,7 +105,7 @@ class CommentController extends Controller
             return response()->json(['status'=>false]);
         }
     }
-    
+
     public function approved(Comment $comment){
         $comment->approved = $comment->approved==0 ? 1 : 0;
         $result = $comment->save();
